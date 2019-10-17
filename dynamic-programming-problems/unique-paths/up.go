d0118558a140b07cfd5f0f4f86372993fa35bc93
mylp <mylp@sendo.vn>
2019-10-17 13:31:46 +0700

    resolved unique path problem

--- dynamic-programming-problems/unique-paths/up.go
+++ dynamic-programming-problems/unique-paths/up.go
@@ -21,12 +21,8 @@ func uniquePaths(m int, n int) int {
 
 // recursive solution
 // func uniquePaths(m int, n int) int {
-// 	if m == 1 && n == 1 {
+// 	if m == 1 || n == 1 {
 // 		return 1
-// 	} else {
-// 		if m == 1 || n == 1 {
-// 			return 1
-// 		}
-// 		return uniquePaths(m-1, n) + uniquePaths(m, n-1)
 // 	}
+// 	return uniquePaths(m-1, n) + uniquePaths(m, n-1)
 // }
